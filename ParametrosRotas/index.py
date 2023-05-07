fb0978a71e89aa48d8af1feff86eefe5ba18fa7d
Gabrielhyds <starkstory6@gmail.com>
2023-05-06 22:19:55 -0300

    Rotas Dinâmicas

---
+++ ParametrosRotas/index.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    #returnando o hello world
+    return "<h2>Hello world</h2>"
+
+@app.route("/sobre")
+def sobre():
+    #returnando o hello world
+    return "<h2>sobre</h2>"
+
+
+@app.route("/noticia/<slug>")
+def noticia(slug):
+    return "Noticia: " + slug
