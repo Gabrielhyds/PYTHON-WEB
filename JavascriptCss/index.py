f53ce3e2a5f11341ea08b9604516e280f894e4d0
Gabrielhyds <starkstory6@gmail.com>
2023-05-06 22:42:06 -0300

    JavaScript e CSS externo

---
+++ JavascriptCss/index.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def html():
+    return render_template("index.html",content=['maca','pera'])
+
+@app.route("/sobre")
+def sobre():
+    #returnando o hello world
+    return "<h2>sobre</h2>"
+
+
+@app.route("/noticia/<slug>")
+def noticia(slug):
+    return "Noticia: " + slug
